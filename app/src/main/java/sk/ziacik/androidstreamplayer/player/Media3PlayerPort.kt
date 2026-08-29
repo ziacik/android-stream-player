a172165f122140abc04499109e99e0264bf1e03e
František Žiačik <ziacik@gmail.com>
2026-08-29 13:06:20 +0200

    feat: add Media3 player adapter

---
+++ app/src/main/java/sk/ziacik/androidstreamplayer/player/Media3PlayerPort.kt
@@ -0,0 +1,27 @@
+package sk.ziacik.androidstreamplayer.player
+
+import android.content.Context
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import sk.ziacik.androidstreamplayer.torrent.TorrentSource
+
+class Media3PlayerPort(context: Context) : PlayerPort {
+    override val player: ExoPlayer = ExoPlayer.Builder(context).build()
+
+    override fun prepare(source: TorrentSource) {
+        player.setMediaItem(MediaItem.fromUri(source.uri))
+        player.prepare()
+    }
+
+    override fun play() {
+        player.play()
+    }
+
+    override fun pause() {
+        player.pause()
+    }
+
+    override fun release() {
+        player.release()
+    }
+}
