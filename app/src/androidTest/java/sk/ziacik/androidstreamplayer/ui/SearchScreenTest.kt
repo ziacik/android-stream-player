ad2990e155ca3e34a789ee015416b5de1b3767b4
František Žiačik <ziacik@gmail.com>
2026-08-29 13:04:39 +0200

    test: fix search screen test imports

--- app/src/androidTest/java/sk/ziacik/androidstreamplayer/ui/SearchScreenTest.kt
+++ app/src/androidTest/java/sk/ziacik/androidstreamplayer/ui/SearchScreenTest.kt
@@ -2,6 +2,7 @@ package sk.ziacik.androidstreamplayer.ui
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
