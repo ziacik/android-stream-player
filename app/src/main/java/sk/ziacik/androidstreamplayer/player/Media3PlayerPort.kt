4a11ec4f8bf90caf1cbe7c2c6be7a074f9dcd3ef
František Žiačik <ziacik@gmail.com>
2026-08-29 15:55:17 +0200

    feat: play torrent bytes through Media3 data source

--- app/src/main/java/sk/ziacik/androidstreamplayer/player/Media3PlayerPort.kt
+++ app/src/main/java/sk/ziacik/androidstreamplayer/player/Media3PlayerPort.kt
@@ -2,14 +2,30 @@ package sk.ziacik.androidstreamplayer.player
 
 import android.content.Context
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DataSource
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import sk.ziacik.androidstreamplayer.torrent.TorrentSource
 
+@UnstableApi
 class Media3PlayerPort(context: Context) : PlayerPort {
     override val player: ExoPlayer = ExoPlayer.Builder(context).build()
 
     override fun prepare(source: TorrentSource) {
-        player.setMediaItem(MediaItem.fromUri(source.uri))
+        val mediaItem = MediaItem.fromUri(source.uri)
+        val pieceAccess = source.pieceAccess
+
+        if (pieceAccess == null) {
+            player.setMediaItem(mediaItem)
+        } else {
+            val dataSourceFactory = DataSource.Factory {
+                TorrentDataSource(pieceAccess)
+            }
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(mediaItem)
+            player.setMediaSource(mediaSource)
+        }
         player.prepare()
     }
 
