1000c21f75795afc0117411768dcd760a388781f
František Žiačik <ziacik@gmail.com>
2026-08-30 02:55:13 +0200

    test: run TorrServer client without Robolectric

--- app/src/test/java/sk/ziacik/androidstreamplayer/torrent/TorrServerClientTest.kt
+++ app/src/test/java/sk/ziacik/androidstreamplayer/torrent/TorrServerClientTest.kt
@@ -10,10 +10,7 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 
-@RunWith(RobolectricTestRunner::class)
 class TorrServerClientTest {
     @Test
     fun streamUrlIsPlayOnly() {
