3fe9aa0b4000ace7ea7d1fd60cc8586896d67f40
František Žiačik <ziacik@gmail.com>
2026-08-30 02:55:37 +0200

    test: use lightweight JSON runtime for JVM tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -148,6 +148,7 @@ dependencies {
 
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.json.jvm)
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
